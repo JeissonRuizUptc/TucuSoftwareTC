7c81a5ff462e2c8f0aa430bf004c697935b125e2
JeissonRuizUPTC <Jeisson.Ruiz01@uptc.edu.co>
2023-10-08 10:35:27 -0500

    TD-END, LOCAL FORMS VALIDITY

--- TUCU-FRONTEND/tucu-frontend/src/App.js
+++ TUCU-FRONTEND/tucu-frontend/src/App.js
@@ -1,11 +1,18 @@
 import react from 'react';
 import LoginPage from './Pages/LoginPage';
 import RegisterPager from './Pages/RegisterPage';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-        <RegisterPager />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element= {<LoginPage/>}></Route>
+            <Route path="/registro" element={<RegisterPager/>}></Route>
+          </Routes>
+        </BrowserRouter>
+
     </div>
   );
 }
