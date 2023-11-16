69e8310e8b3d10590283d33db7384c7e8774c748
JeissonRuizUPTC <Jeisson.Ruiz01@uptc.edu.co>
2023-11-16 01:13:11 -0500

    List Estable

--- TUCU-FRONTEND/tucu-frontend/src/Components/DeliveryTableItem.jsx
+++ TUCU-FRONTEND/tucu-frontend/src/Components/DeliveryTableItem.jsx
@@ -6,21 +6,24 @@ const DeliveryTableItem = ({ index, delivery }) => {
   const [buttonText, setButtonText] = useState("");
   const [buttonColor, setButtonColor] = useState("green");
   const [remainingTime, setRemainingTime] = useState(delivery.preparation_time * 60);
+  const [deliveryState, setDeliveryState] = useState(delivery.state);
 
   const handleButtonClick = async () => {
-    try {
-      if (delivery.state === 1 && buttonText === "Pedido listo") {
-        // Realiza la solicitud PUT al endpoint cuando el botón está en "Pedido listo"
-        await updateDeliveryState(delivery.idDELIVERIES, 2);
-        setButtonText("ENTREGADO");
-        setButtonColor("green");
-      } else if (buttonText === "ENTREGADO") {
-        // Realiza la solicitud PUT al endpoint cuando el botón está en "ENTREGADO"
-        await updateDeliveryState(delivery.idDELIVERIES, 3);
-        setButtonText("");
-      }
-    } catch (error) {
-      console.error("Error al actualizar el estado de la entrega:", error);
+    if (deliveryState === 1) {
+      // Si el estado es 1, actualiza a EN CAMINO y establece el tiempo restante a 0
+      setDeliveryState(2);
+      setRemainingTime(0);
+      setButtonText("ENTREGADO");
+      setButtonColor("yellow");
+      // Realiza la solicitud PUT al endpoint con el nuevo estado
+      await updateDeliveryState(delivery.idDELIVERIES, 2);
+    } else if (buttonText === "ENTREGADO") {
+      // Si el botón muestra ENTREGADO, actualiza a ENTREGADO y establece el tiempo restante a 0
+      setDeliveryState(3);
+      setRemainingTime(0);
+      setButtonText("");
+      // Realiza la solicitud PUT al endpoint con el nuevo estado
+      await updateDeliveryState(delivery.idDELIVERIES, 3);
     }
   };
 
@@ -35,51 +38,55 @@ const DeliveryTableItem = ({ index, delivery }) => {
 
   useEffect(() => {
     // Actualiza el texto y color del botón según el estado inicial
-    console.log(delivery.state)
-    if (delivery.state === 1) {
+    if (deliveryState === 1) {
       setButtonText("Pedido listo");
       setButtonColor("green");
-    } else if (delivery.state === 2) {
+    } else if (deliveryState === 2) {
       setButtonText("ENTREGADO");
-      setButtonColor("green");
-    } else if (delivery.state === "EN CAMINO") {
-      setButtonText("hptaaa");
+      setButtonColor("yellow");
     }
-  }, [delivery.state]);
+  }, [deliveryState]);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setRemainingTime((prevTime) => (prevTime > 0 ? prevTime - 1 : 0));
     }, 1000);
 
-    if (remainingTime === 0) {
+    if (remainingTime === 0 && deliveryState === 1) {
+      // Cuando el temporizador llega a 0 y el estado es 1, actualiza a EN CAMINO y establece el botón a ENTREGADO
       clearInterval(intervalId);
-
-      if (delivery.state === 1) {
-        // Actualiza el estado a "EN CAMINO" y realiza la solicitud PUT al endpoint
-        setButtonText("ENTREGADO");
-        setButtonColor("green");
-        updateDeliveryState(delivery.idDELIVERIES, 2);
-      }
+      setDeliveryState(2);
+      setButtonText("ENTREGADO");
+      setButtonColor("yellow");
+      // Realiza la solicitud PUT al endpoint con el nuevo estado
+      updateDeliveryState(delivery.idDELIVERIES, 2);
     }
 
     return () => clearInterval(intervalId);
-  }, [remainingTime, delivery.idDELIVERIES, delivery.state]);
+  }, [remainingTime, delivery.idDELIVERIES, deliveryState]);
 
   return (
     <tr>
       <td>{index + 1}</td>
       <td>{new Date(delivery.timestamp).toLocaleTimeString()}</td>
       <td>
-        <TemporizadorComponent preparationTime={delivery.preparation_time} startTime={delivery.timestamp} />
+        <TemporizadorComponent
+          preparationTime={delivery.preparation_time}
+          startTime={delivery.timestamp}
+          remainingTime={remainingTime}
+          setRemainingTime={(time) => {
+            setRemainingTime(time);
+          }}
+          deliveryState={deliveryState}
+        />
       </td>
-      <td>{remainingTime}</td>
+      <td>00:00:00</td>
       <td>{delivery.DELIVERYMEN.surname}</td>
       <td>{delivery.address}</td>
       <td>
-        {delivery.state === 1 ? "EN PREPARACIÓN" :
-          delivery.state === 2 ? "EN CAMINO" :
-            delivery.state === 3 ? "ENTREGADO" : ""}
+        {deliveryState === 1 ? "EN PREPARACIÓN" :
+          deliveryState === 2 ? "EN CAMINO" :
+            deliveryState === 3 ? "ENTREGADO" : ""}
       </td>
       <td>
         {buttonText && (
